53528b412f0244ffea59b4d44ca5c5b2ae3dfab8
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-08-01 17:29:52 +0100

    Add project files.

---
+++ BankSafeVirtualOverrideTest/JewelThief.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSafeVirtualOverrideTest
+{
+    class JewelThief : Locksmith
+    {
+        private string stolenJewels;
+        protected override void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine($"The jewels are mine! I stole: {stolenJewels}");
+        }
+    }
+}
